718936eceef81eb87f5cff7c7dc33a8df91979ce
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-06-02 21:32:53 +0530

    Quick sort

---
+++ quick_sort.py
@@ -0,0 +1,18 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+# Example usage
+arr = [9, 4, 2, 7, 1, 5, 3, 8, 6]
+sorted_arr = quick_sort(arr)
+
+print("Original array:", arr)
+print("Sorted array:", sorted_arr)
